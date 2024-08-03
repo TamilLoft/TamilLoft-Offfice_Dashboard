7cb3c5bbd2d16d1007fb3a41f014ab14e3481cd0
Luvin-Max <tamilloft@gmail.com>
2024-08-03 20:21:54 +0530

    luvin commit

---
+++ office-dashboard-backend/config/corsOptions.js
@@ -0,0 +1,11 @@
+const WhiteList = ['https://www.google.com','http://localhost:5500/']
+const Acces ={
+    origin:(origin,callback)=>{
+        if(WhiteList.indexOf(origin) !== -1 || !origin){
+            callback(null,true);
+        }else{
+            callback(new Error ('not allowed in this web site'));
+        }
+    },
+    optionSuccessStatus:200
+}
